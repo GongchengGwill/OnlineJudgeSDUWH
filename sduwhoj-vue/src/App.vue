cada2ad11a2137282b31786ded32fd636f51f187
GongchengGwill <1092177525@qq.com>
2025-02-26 19:54:45 +0800

    前端项目创建

---
+++ sduwhoj-vue/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <MainHome />
+  </div>
+</template>
+<script setup lang="ts">
+
+import MainHome from "@/MainPage/Home/MainHome.vue";
+</script>
+import MainHome from "/src/MainPage/Home/MainHome.vue";
+<style></style>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
